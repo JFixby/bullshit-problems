5e4c148572eeb40e86907bedffd9cc4743da720f
J Fixby <github@jfixby.com>
2016-04-29 22:46:26 +0200

    max

--- com.hackerrank.java/solutions/Maximus/Solution.java
+++ com.hackerrank.java/solutions/Maximus/Solution.java
@@ -15,8 +15,42 @@ public class Solution extends AbstractSolution {
 	public static void main (final String[] args) {
 		final Scanner in = new Scanner(input);
 
-		log(3);
+		final int arraySize = in.nextInt();
+		final int numberOfQuries = in.nextInt();
 
+		final int[] A = new int[arraySize];
+		for (int i = 0; i < arraySize; i++) {
+			final int value = in.nextInt();
+			A[i] = value;
+		}
+
+// log(Arrays.toString(A));
+		for (int i = 0; i < numberOfQuries; i++) {
+			final int L = in.nextInt();
+			final int R = in.nextInt();
+			final long value = F(A, L, R);
+			log(value);
+		}
+
+	}
+
+	private static long F (final int[] A, final int L, final int R) {
+		long result = 0;
+		for (int l = L; l <= R; l++) {
+			for (int r = l; r <= R; r++) {
+				result = result + max(A, l, r);
+			}
+		}
+
+		return result;
+	}
+
+	private static long max (final int[] A, final int l, final int r) {
+		long max = A[l - 1];
+		for (int i = l - 1; i <= r - 1; i++) {
+			max = Math.max(max, A[i]);
+		}
+		return max;
 	}
 
 }
