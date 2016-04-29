2b4311f967f49de8523fbc558489b7ca3937e354
J Fixby <github@jfixby.com>
2016-04-29 19:23:59 +0200

    ...

---
+++ com.hackerrank.java/solutions/Coins/RunCoins.java
@@ -0,0 +1,14 @@
+
+package Coins;
+
+import com.jfixby.hrank.SolutionRunner;
+import com.jfixby.red.desktop.DesktopAssembler;
+
+public class RunCoins {
+
+	public static void main (final String[] args) throws Throwable {
+		DesktopAssembler.setup();
+		SolutionRunner.run(Coins.Solution.class);
+	}
+
+}
---
+++ com.hackerrank.java/solutions/KMP/RunKMP.java
@@ -0,0 +1,14 @@
+
+package KMP;
+
+import com.jfixby.hrank.SolutionRunner;
+import com.jfixby.red.desktop.DesktopAssembler;
+
+public class RunKMP {
+
+	public static void main (final String[] args) throws Throwable {
+		DesktopAssembler.setup();
+		SolutionRunner.run(KMP.Solution.class);
+	}
+
+}
