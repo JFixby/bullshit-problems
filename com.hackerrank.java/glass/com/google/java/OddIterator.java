070939b5abbe2b4f9530c806fbad52f45338ecba
J Fixby <github@jfixby.com>
2016-07-28 11:11:08 +0200

    ...

--- com.hackerrank.java/glass/com/google/java/OddIterator.java
+++ com.hackerrank.java/glass/com/google/java/OddIterator.java
@@ -4,22 +4,31 @@ package com.google.java;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import com.jfixby.cmns.api.debug.Debug;
 import com.jfixby.cmns.api.log.L;
 import com.jfixby.red.desktop.DesktopSetup;
 
 public class OddIterator<T> implements Iterator<T> {
-// original [0,1,2,3,4,5,6,7,8]
-// window {0,X} {2,X}, {4,X}, {6,X}, {8}//over
+// original [0,#1,2,3,>#4,5,6,#7>] window size = 3, index in window == 1 (middle)
+//
 
 	private final Iterator<T> origignal;
 	int windowSize = 2;
-	int indexInWindow = this.windowSize - 1;// +index in window, last
+// int indexInWindow = this.windowSize - 1;// +index in window, last
+	int indexInWindow = 0;// +index in window, last
 	boolean hasNext = true;
 	T nextElement = null;
 	// +queue -queue, +windowSize +hasNex -hasNext
 
-	public OddIterator (final Iterator<T> iterator) {
+	public OddIterator (final Iterator<T> iterator, final int windowSize, final int indexInWindow) {
 		this.origignal = iterator;
+		this.windowSize = windowSize;// check >0
+		this.indexInWindow = indexInWindow;
+
+		Debug.checkTrue(this.windowSize > 0);
+		Debug.checkTrue(this.indexInWindow >= 0);
+		Debug.checkTrue(this.windowSize > this.indexInWindow);
+
 		this.shiftWindow(this.windowSize);
 	}
 
@@ -32,6 +41,8 @@ public class OddIterator<T> implements Iterator<T> {
 					this.nextElement = e;
 					this.hasNext = true;
 				}
+			} else {
+				break;
 			}
 		}
 	}
@@ -46,23 +57,19 @@ public class OddIterator<T> implements Iterator<T> {
 		return next;
 	}
 
-	private void throwError () {
-	}
-
 	@Override
 	public boolean hasNext () {
-		// return this.windowContent.size() > this.indexInWindow;// >0,==window size, >=indexInWindow,///optimise by discarding
-		// heads?
-		// window contains target index
-		// return this.windowContent.size() == 1;// >0,==window size, >=indexInWindow,///optimise by discarding heads?
-		// window contains target index
 		return this.hasNext;
+	}
+
+	private void throwError () {
+		throw new Error();
 	}
 
 	public static void main (final String[] args) {
 		DesktopSetup.deploy();
 		final ArrayList<Float> original = new ArrayList<Float>();
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 7; i++) {
 			original.add(i * 1f);
 		}
 
@@ -71,13 +78,13 @@ public class OddIterator<T> implements Iterator<T> {
 		}
 
 		for (final Iterator<Float> it = wrap(original.iterator()); it.hasNext();) {
-			L.d("x = " + it.next());
+			L.d("y = " + it.next());
 		}
 
 	}
 
 	private static Iterator<Float> wrap (final Iterator<Float> iterator) {
-		return new OddIterator<Float>(iterator);
+		return new OddIterator<Float>(iterator, 3, 1);
 	}
 
 }
