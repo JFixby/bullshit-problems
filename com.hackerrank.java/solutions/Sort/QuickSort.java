db735e5427c4fccb86f996beeec9a16988163161
J Fixby <github@jfixby.com>
2016-09-01 13:44:38 +0200

    ...

--- com.hackerrank.java/solutions/Sort/QuickSort.java
+++ com.hackerrank.java/solutions/Sort/QuickSort.java
@@ -35,32 +35,44 @@ public class QuickSort {
 	}
 
 	private static void sort (final int[] array, final int fromIndex, final int toIndex) {
-		L.d("sort " + fromIndex, toIndex);
-		if (fromIndex >= toIndex) {
+// L.d("sort " + fromIndex, toIndex);
+// L.d(Arrays.toString(array));
+		final int numberOfElements = toIndex - fromIndex + 1;
+
+		if (numberOfElements <= 1) {
+			return;
+		}
+
+		if (numberOfElements == 2) {
+			if (array[fromIndex] >= array[toIndex]) {
+				swap(fromIndex, toIndex, array);
+			}
 			return;
 		}
 
 // int middleIndex = fromIndex + (toIndex - fromIndex) / 2;
 
 		final int pivot = array[fromIndex];
-
-		int leftIndex = fromIndex;
+// L.d("pivot", pivot);
+		int leftIndex = fromIndex + 1;
 		int rightIndex = toIndex;
 
-		while (leftIndex < rightIndex) {
+		while (leftIndex <= rightIndex) {
 			final int leftValue = array[leftIndex];
 			if (leftValue < pivot) {
 				leftIndex++;
 			} else {
 				swap(leftIndex, rightIndex, array);
 				rightIndex--;
+// L.d(leftValue + " > " + Arrays.toString(array));
 			}
 		}
 
-		final int middleIndex = leftIndex;
-
+		final int middleIndex = rightIndex;
+		swap(fromIndex, middleIndex, array);
+// L.d(" i " + Arrays.toString(array));
 		sort(array, fromIndex, middleIndex);
-		sort(array, middleIndex, toIndex);
+		sort(array, middleIndex + 1, toIndex);
 
 	}
 
@@ -79,7 +91,7 @@ public class QuickSort {
 		final int[] result = new int[size];
 		final java.util.Random r = new java.util.Random(0);
 		for (int i = 0; i < size; i++) {
-			result[i] = r.nextInt();
+			result[i] = r.nextInt() * 1;
 		}
 		return result;
 	}
