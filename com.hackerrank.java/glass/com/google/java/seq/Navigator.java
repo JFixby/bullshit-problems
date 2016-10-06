4b846decf70f199d0170f89c0040feea5eab87fb
J Fixby <github@jfixby.com>
2016-10-06 20:02:58 +0200

    ...

--- com.hackerrank.java/glass/com/google/java/seq/Navigator.java
+++ com.hackerrank.java/glass/com/google/java/seq/Navigator.java
@@ -11,7 +11,9 @@ public class Navigator {
 	public static void main (final String[] args) {
 		DesktopSetup.deploy();
 
-		final int[] map = new int[] {3, 4, 1, 1, 0, 2, 2, 0, 1, 1};
+// final int[] map = new int[] {3, 4, 1, 1, 0, 2, 2, 0, 1, 1};
+// final int[] map = new int[] {2, 2, 2, 1, 1};
+		final int[] map = new int[] {2, 3, 1, 1, 4};
 
 		final int startIndex = 0;
 		final int endIndex = map.length - 1;
@@ -25,17 +27,32 @@ public class Navigator {
 			L.d(" from <" + currentIndex + "> follow the sign '" + roadSign + "'");
 			currentIndex = roadSign.direction;
 		}
+		navigator.printStats();
+	}
+
+	private void printStats () {
+		L.d("calls done", this.CALL);
+		L.d("memory used", this.MEMORY);
+		int sum = 0;
+		for (int i = 0; i < this.map.length; i++) {
+			sum = sum + this.map[i];
+		}
+		L.d("sum", sum);
+
 	}
 
 	private final int[] map;
 	private final λ hops;
+	private long CALL;
+	private long MEMORY;
 
 	public Navigator (final int[] map) {
 		this.map = map;
-		this.hops = memoization(this.setupLazyλExpression());
+		this.hops = this.memoization(this.setupLazyλExpression());
 	}
 
 	public RoadSign getDirection (final int startIndex, final int endIndex) {
+
 		return this.hops.evaluate(startIndex, endIndex);
 	}
 
@@ -44,7 +61,7 @@ public class Navigator {
 			if (from >= to) {
 				return new RoadSign(from, 0);// arrived
 			}
-
+			this.CALL++;
 			final int mapDirections = this.map[from];
 
 			final RoadSign worstOption = new RoadSign(from, Integer.MAX_VALUE);
@@ -56,6 +73,9 @@ public class Navigator {
 				if (option.distance < bestOption.distance) {
 					bestOption = option;
 					bestConnection = via;
+					if (bestOption.distance == 0) {
+						break;
+					}
 				}
 			}
 
@@ -65,14 +85,18 @@ public class Navigator {
 		};
 	}
 
-	static λ memoization (final λ expr) {
+	λ memoization (final λ expr) {
 		final HashMap<String, RoadSign> cache = new HashMap<String, RoadSign>();
 		return (from, to) -> {
+
 			final String key = from + "->" + to;
 			RoadSign value = cache.get(key);
 			if (value == null) {
+
 				value = expr.evaluate(from, to);
 				cache.put(key, value);
+				L.e("    store", key + " : " + value);
+				this.MEMORY++;
 			}
 			return value;
 		};
